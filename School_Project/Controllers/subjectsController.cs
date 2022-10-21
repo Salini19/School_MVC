f07581a4ee28206bae54394a0709d61b3f0dfbfc
Salini <shalini021037@gmail.com>
2022-10-21 11:51:04 +0530

    Add project files.

---
+++ School_Project/Controllers/subjectsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using School_Project.Models;
+
+namespace School_Project.Controllers
+{
+    public class subjectsController : Controller
+    {
+        private SchoolEntities1 db = new SchoolEntities1();
+
+        // GET: subjects
+        public ActionResult Index()
+        {
+            var subjects = db.subjects.Include(s => s.Class);
+            return View(subjects.ToList());
+        }
+
+        // GET: subjects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            subject subject = db.subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // GET: subjects/Create
+        public ActionResult Create()
+        {
+            ViewBag.classId = new SelectList(db.Classes, "Classid", "ClassName");
+            return View();
+        }
+
+        // POST: subjects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SubjectId,SubjectName,classId")] subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.subjects.Add(subject);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.classId = new SelectList(db.Classes, "Classid", "ClassName", subject.classId);
+            return View(subject);
+        }
+
+        // GET: subjects/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            subject subject = db.subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.classId = new SelectList(db.Classes, "Classid", "ClassName", subject.classId);
+            return View(subject);
+        }
+
+        // POST: subjects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SubjectId,SubjectName,classId")] subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subject).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.classId = new SelectList(db.Classes, "Classid", "ClassName", subject.classId);
+            return View(subject);
+        }
+
+        // GET: subjects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            subject subject = db.subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // POST: subjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            subject subject = db.subjects.Find(id);
+            db.subjects.Remove(subject);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
